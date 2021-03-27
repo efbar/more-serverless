99d462619a0f14e57826ab7d011bcc62079f8886
efbar <17676301+efbar@users.noreply.github.com>
2021-03-27 16:11:05 +0100

    fix path

--- nomad-jobs-list/handler.go
+++ nomad-jobs-list/handler.go
@@ -3,7 +3,7 @@ package function
 import (
 	"net/http"
 
-	nomadjobs "github.com/efbar/more-serverless/nomad-list-jobs/nomadjobs"
+	nomadjobs "github.com/efbar/more-serverless/nomad-jobs-list/nomadjobs"
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
